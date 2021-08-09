785257378e877b4c229b0275c9a9903997bf1d82
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2021-08-09 10:55:23 +0200

    Bug fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,15 @@ import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import News from '../pages/News/News';
 import Contact from '../pages/Contact/Contact';
+
 import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
-  } from "react-router-dom";
+    Link,
+    Redirect
+} from "react-router-dom";
+
 import Logo from "./logo.png";
 
 class Navbar extends React.Component {
@@ -16,29 +19,40 @@ class Navbar extends React.Component {
         return (
             <Router>
                 <div style={ { textAlign: "center", padding: "2rem 0" } }>
-                    <img src={Logo} alt="Logo" style={ { maxWidth: "120px" } }></img>
+                    <img src={Logo} alt="Logo" style={ { maxWidth: "120px" } }/>
                 </div>
                 <div>
                     <nav id="nav__main">
                         <ul>
-                            <li><Link to="/home">Home</Link></li>
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/news">News</Link></li>
-                            <li><Link to="/contact">Contact</Link></li>
+                            <li>
+                                <Link to="/home">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/news">News</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
                         </ul>
                     </nav>
                     <Switch>
-                        <Route path="/about">
+                        <Route exact path="/home">
+                            <Home />
+                        </Route>
+                        <Route exact path="/about">
                             <About />
                         </Route>
-                        <Route path="/news">
+                        <Route exact path="/news">
                             <News />
                         </Route>
-                        <Route path="/contact">
+                        <Route exact path="/contact">
                             <Contact />
                         </Route>
                         <Route path="/">
-                            <Home />
+                            <Redirect to="/home" />
                         </Route>
                     </Switch>
                 </div>
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-import Welcome from '../pages/Welcome/Welcome';
 import "./Main.css";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
 
 class MainPage extends React.Component {
     render() {
